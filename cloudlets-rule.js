f6aa04d209cf770a23ddd766106737ba32f477c2
NITECO\hung.dang <hung.dang@niteco.se>
2024-09-23 09:46:45 +0700

    Refactor cloudlets and add comments

---
+++ cloudlets-rule.js
@@ -0,0 +1,161 @@
+const puppeteer = require('puppeteer');
+const log = require('./log')
+
+/**
+ * When you are in 'Version Details' page and clicked '...' icon in one rule row has matched `ruleName` to open the menu
+ * 
+ * you can choose the action item by name `menuItemText` in given menu
+ * @param {*} page 
+ * @param {*} ruleName
+ * @param {*} menuItemText 
+ */
+const clickToMenuItemInVersionDetailsPage = async (page, ruleName, menuItemText) => {
+    const rowContainsMenuXpath = `xpath=//div[contains(@class, "akamaiTableRules")]//table/tbody/tr[td[contains(@class, "name-column")][contains(string(), "${ruleName}")]]/td[last()]/div`
+    const matchItemPath = `${rowContainsMenuXpath}//ul[@class="dropdown-menu"]/li[contains(string(),"${menuItemText}")]`
+    await page.locator(matchItemPath)
+        .on(puppeteer.LocatorEvent.Action, () => {
+            log.white(`Click to item ${menuItemText} in given menu`)
+        })
+        .click();
+}
+
+var self = module.exports = {
+
+    /**
+     * When you are in 'Version Details' page, 
+     * 
+     * you click '...' in the rule row has match `ruleName` to open the rule details form
+     * @param {*} page 
+     * @param {*} ruleName 
+     */
+    openTheRuleFormByNameInVersionDetails: async (page, ruleName) => {
+        await page.locator('xpath=//div[contains(@class, "akamaiTableRules")]//table/tbody/tr').wait();
+
+        const xpthSearchInput = `xpath=//div[contains(@class, "akamaiTableRules")]//input[@type="search"]`
+        await page.locator(xpthSearchInput)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Find the rule by name: '${ruleName}'`)
+            })
+            .fill(ruleName)
+
+        const matchRule = `xpath=//div[contains(@class, "akamaiTableRules")]//table/tbody/tr[td[contains(@class, "name-column")][contains(string(), "${ruleName}")]]/td[last()]/div`
+        await page.locator(matchRule)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Click to '...' icon to open the menu in the: '${ruleName}' rule row`)
+            }).click()
+
+        clickToMenuItemInVersionDetailsPage(page, ruleName, "Edit Rule")
+    },
+
+    /**
+     * When you are in 'Version Details' page, 
+     * 
+     * you click to 'Add Rule' button to open the new rule form and fill the basic information
+     * 
+     * such as rule's name, source url, redirect url, redirect type, isCopyQueryString and relativeRedirectOption
+     * @param {*} page 
+     * @param {*} ruleName 
+     * @param {*} sourceUrl 
+     * @param {*} redirectURL 
+     * @param {*} redirectType 
+     * @param {*} copyQueryString 
+     * @param {*} relativeRedirectOption 
+     */
+    addNewBasicRule: async (page, ruleName, sourceUrl, redirectURL, redirectType, copyQueryString, relativeRedirectOption = undefined) => {
+        await page.locator('xpath=//akam-table-toolbar//button[contains(string(), "Add Rule")]')
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Click to 'Add Rule' button`)
+            }).click()
+
+        //fill rule form
+        await page.locator(`xpath=//form[@name="ruleForm"]//rule-settings-name//input[@name="ruleName"]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Fill the rule name '${ruleName}'`)
+            }).fill(ruleName)
+        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//input[@name="sourceURL"]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Fill the source URL '${sourceUrl}'`)
+            }).fill(sourceUrl)
+        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//input[@name="redirectURL"]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Fill the redirect URL '${redirectURL}'`)
+            }).fill(redirectURL)
+        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//select[@name="redirectType"]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Select the redirect type '${redirectType}'`)
+            }).fill(redirectType)
+
+        if (copyQueryString) {
+            await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//rule-settings-copy-query-params//input[@type="checkbox"]/following-sibling::div`)
+                .on(puppeteer.LocatorEvent.Action, () => {
+                    log.white(`Checked to Copy Query String checkbox`)
+                })
+                .click()
+        }
+
+        if (relativeRedirectOption) {
+            const xpathRelativeOption = `xpath=//form[@name="ruleForm"]//div[@id="rule-settings-use-relative-url"]//select[@name="relativeURL"]`
+            let optionValue = await page.$$eval(`${xpathRelativeOption}/option`, (options, text) => options.find(o => o.innerText === text).value, relativeRedirectOption)
+            await page.on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Select Relative Redirect Options: ${optionValue}`)
+            }).select(xpathRelativeOption, optionValue);
+        }
+
+    },
+
+    /**
+     * When you opened the Add Rule form dialog, you can click to show Advanced View
+     * @param {*} page 
+     */
+    showAdvancedViewInRuleForm: async (page) => {
+        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="modalButtonDiv"]//button[contains(@class, "switchDetailView")]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Click to 'Show Advanced View'`)
+            }).click()
+    },
+
+    updateRuleMatchInAdvancedView: async (page, matchName, matchOperator, matchValue, index) => {
+        const matchPath = `xpath=//div[@id="rule_details_advanced"]//div[contains(@class, "updateableList")]/div[${index}]`
+        const matchNamePath = `${matchPath}//ng-form//select`
+        var optionValue = await page.$$eval(`${matchNamePath}/option`, (options, text) => options.find(o => o.innerText === text).value, matchName)
+        await page.select(matchNamePath, optionValue);
+        log.white(`Select Rule Match Name: ${matchName}`)
+
+        const matchOperatorPath = `${matchPath}//match-operator//select`
+        let operatorOptions = await page.$$eval(`${matchOperatorPath}/option`, (options, text) => options.find(o => o.innerText === text).value, matchOperator)
+        await page.select(matchOperatorPath, operatorOptions);
+        log.white(`Select Rule Match Operator: ${matchOperator}`)
+
+        const matchValuePath = `${matchPath}//match-multi-value//input[@type="text"]`
+        await page.locator(matchValuePath)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Fill the rule match value: ${matchValue}`)
+            }).fill(matchValue);
+        await page.keyboard.press('Enter')
+
+        //const checkbox1 = `//div[@id="rule_details_advanced"]//div[contains(@class, "updateableList")]/div[1]//match-multi-value//input[@type="checkbox"]/following-sibling::div[contains(string(), "Allow wildcards * (zero or more) and ? (any character)")]`
+    },
+    
+    /**
+     * Need to refactor this function to support edit any field in rule form
+     * @param {*} page 
+     * @param {*} redirectURL 
+     */
+    editRedirectUrlInRuleForm: async (page, redirectURL) => {
+        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//input[@name="redirectURL"]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Fill the redirect URL '${redirectURL}'`)
+            }).fill(redirectURL)
+    },
+
+    /**
+     * When you are on the Rule form dialog, you can click to 'Save Rule' button to add rule into current version
+     * @param {*} page 
+     */
+    saveRuleInRuleForm: async (page) => {
+        await page.locator(`xpath=//div[@id="modalButtonDiv"]//button[@id="saveRule"]`)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.yellow(`Click to 'Save Rule' button`)
+            }).click();
+    },
+}
--- cloudlets.js
+++ cloudlets.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const log = require('./log')
+const Rule = require('./cloudlets-rule');
 
 const getPolicyVersionNumberBasedOnEnvironment = async (page, environment) => {
     const xpathStaging = `xpath=//div[@class="infoBlockAccordion"]//div[contains(@class, "panel-heading")]//div[span[string()="${environment}"]]/span[2]`
@@ -9,39 +10,77 @@ const getPolicyVersionNumberBasedOnEnvironment = async (page, environment) => {
         }).click();
 
     await new Promise(r => setTimeout(r, 1000));
-    const stagingVersion = await page.$eval(xpathStaging, el => el.innerText);
-    return stagingVersion;
+    const versionNumber = await page.$eval(xpathStaging, el => el.innerText);
+    return versionNumber;
 }
 
+/**
+ * When you are in 'Policy Details' page and clicked '...' icon in one version row has matched `versionNumber` to open the menu
+ * 
+ * you can choose the action item by name `menuItemText` in given menu
+ * @param {*} page 
+ * @param {*} versionNumber
+ * @param {*} menuItemText 
+ */
+const clickToMenuItemInPolicyDetailsPage = async (page, versionNumber, menuItemText) => {
+    const xpathTdIcon = `xpath=//akam-table//table//tr[td[@row-property="version"][contains(string(), "${versionNumber}")]]/td[last()]/div`
+    const matchItemPath = `${xpathTdIcon}//ul[@class="dropdown-menu"]/li[contains(string(),"${menuItemText}")]`
+    await page.locator(matchItemPath)
+        .on(puppeteer.LocatorEvent.Action, () => {
+            log.white(`Click to item ${menuItemText} in given menu`)
+        })
+        .click();
+}
+
+
 var self = module.exports = {
-    goToCloudletsByName: async (page, cloudletsName) => {
+    Rule,
+
+    /**
+     * When you are in 'Cloudlets Policy Manager' page, you can navigate to 'Policy Details' page by searching by policy name
+     * @param {*} page 
+     * @param {*} policyName 
+     */
+    goToCloudletsPolicyByName: async (page, policyName) => {
         await page.locator('xpath=//akam-table[@id-property="policyId"]//table/tbody/tr').wait();
 
         const xpthSearchInput = `//div[contains(@class, "cloudletsView")]//input[@type="search"]`
         await page.locator('xpath=' + xpthSearchInput)
             .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill in search box: '${cloudletsName}'`)
+                log.white(`Fill in search box with policy name: '${policyName}'`)
             })
-            .fill(cloudletsName)
+            .fill(policyName)
 
-        const matchResult = `//div[contains(@class, "cloudletsView")]//table//tr[td[contains(string(), "${cloudletsName}")]]/td[1]//a`
+        const matchResult = `//div[contains(@class, "cloudletsView")]//table//tr[td[contains(string(), "${policyName}")]]/td[1]//a`
         await page.locator('xpath=' + matchResult)
             .on(puppeteer.LocatorEvent.Action, () => {
-                log.yellowBg(`Click to cloudlets: '${cloudletsName}'`)
+                log.yellowBg(`Click to policy: '${policyName}'`)
             }).click()
         await page.waitForNavigation();
     },
 
+    /**
+     * When you are in 'Policy Details' page, you can get the staging version number
+     * @param {*} page The Puppeteer's page object
+     * @returns 
+     */
     getStagingPolicyVersionNumber: async (page) => {
         return await getPolicyVersionNumberBasedOnEnvironment(page, "Staging");
     },
+
+    /**
+     * When you are in 'Policy Details' page, you can get the production version number
+     * @param {*} page The Puppeteer's page object
+     * @returns 
+     */
     getProductionPolicyVersionNumber: async (page) => {
         return await getPolicyVersionNumberBasedOnEnvironment(page, "Production");
     },
+
     /**
-     * When you are in Property page, you can check if there has a draft version based on other version number
+     * When you are in 'Policy Details' page, you can check if there has a draft version based on other version number
      * @param {*} page The Puppeteer's page object
-     * @param {*} baseVersionNumber The base version number, ex 'Version 40'
+     * @param {*} baseVersionNumber The base version number, ex 'v40'
      * @returns 
      */
     checkHasDraftVersionBasedOnOtherVersionNumber: async (page, baseVersionNumber) => {
@@ -50,6 +89,12 @@ var self = module.exports = {
         return hasDraftVersion;
     },
 
+    /**
+     * When you are in 'Policy Details' page, you can navigate to the latest draft version based on other version number
+     * @param {*} page The Puppeteer's page object
+     * @param {*} baseVersionNumber The base version number, ex 'v40'
+     * @returns 
+     */
     goToLatestDraftVersionBasedOnVersionNumber: async (page, baseVersionNumber) => {
         const xpathVersion = `//akam-table//table//tr[td[@row-property="description"][contains(string(), "${baseVersionNumber}")]]/td[1]/a[count(i) = 0]`
         const versionName = await page.$eval('xpath=' + xpathVersion, el => el.innerText);
@@ -61,19 +106,41 @@ var self = module.exports = {
         await page.waitForNavigation();
     },
 
+    /**
+     * When you are in 'Policy Details' page, you can navigate to the version details page based on version number
+     * @param {*} page The Puppeteer's page object
+     * @param {*} versionNumber The base version number, ex 'v40'
+     * @returns 
+     */
+    goToVersionDetailsBasedOnVersionNumber: async (page, versionNumber) => {
+        const xpathIcon = `xpath=//akam-table//table//tr/td[@row-property="version"][contains(string(), "${versionNumber}")]/a`
+        await page.locator(xpathIcon)
+            .on(puppeteer.LocatorEvent.Action, () => {
+                log.white(`Click to open the policy: '${versionNumber}'`)
+            }).click()
+
+        await page.waitForNavigation();
+    },
+
+    /**
+     * When you are in 'Policy Details' page, you can create the new policy version based on previous version
+     * @param {*} page 
+     * @param {*} baseVersionNumber 
+     * @param {*} versionNote 
+     */
     createNewVersionBasedOnVersionNumber: async (page, baseVersionNumber, versionNote) => {
         const xpathIcon = `xpath=//akam-table//table//tr[td[@row-property="version"][contains(string(), "${baseVersionNumber}")]]/td[last()]/div`
         await page.locator(xpathIcon)
             .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Click to open the menu of policy: '${baseVersionNumber}'`)
+                log.white(`Click '...' to open the menu of based policy version '${baseVersionNumber}' row`)
             }).click()
 
-        await self.clickToItemInDropdown(page, baseVersionNumber, "Edit New Version")
+        await clickToMenuItemInPolicyDetailsPage(page, baseVersionNumber, "Edit New Version")
 
         const xpathNotes = `xpath=//div[@class="policyVersionModal"]//textarea[@name="notes"]`
         await page.locator(xpathNotes)
             .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill the notes policy: '${versionNote}'`)
+                log.white(`Fill the policy version's notes: '${versionNote}'`)
             }).fill(`${versionNote} (Based on ${baseVersionNumber})`)
 
         const xpathSaveBtn = `xpath=//div[@class="modal-footer"]/button[contains(string(), "Edit New Version")]`
@@ -84,67 +151,12 @@ var self = module.exports = {
         await page.waitForNavigation();
     },
 
-    openAndFillBasicRuleForm: async (page, ruleName, sourceUrl, redirectURL, redirectType, copyQueryString, relativeRedirectOption = undefined) => {
-        await page.locator('xpath=//akam-table-toolbar//button[contains(string(), "Add Rule")]')
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Click to 'Add Rule' button`)
-            }).click()
-
-        //fill rule form
-        await page.locator(`xpath=//form[@name="ruleForm"]//rule-settings-name//input[@name="ruleName"]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill the rule name '${ruleName}'`)
-            }).fill(ruleName)
-        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//input[@name="sourceURL"]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill the source URL '${sourceUrl}'`)
-            }).fill(sourceUrl)
-        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//input[@name="redirectURL"]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill the redirect URL '${redirectURL}'`)
-            }).fill(redirectURL)
-        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//select[@name="redirectType"]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Select the redirect type '${redirectType}'`)
-            }).fill(redirectType)
-
-        if (copyQueryString) {
-            await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//rule-settings-copy-query-params//input[@type="checkbox"]/following-sibling::div`)
-                .on(puppeteer.LocatorEvent.Action, () => {
-                    log.white(`Checked to Copy Query String checkbox`)
-                })
-                .click()
-        }
-
-        if (relativeRedirectOption) {
-            const xpathRelativeOption = `xpath=//form[@name="ruleForm"]//div[@id="rule-settings-use-relative-url"]//select[@name="relativeURL"]`
-            let optionValue = await page.$$eval(`${xpathRelativeOption}/option`, (options, text) => options.find(o => o.innerText === text).value, relativeRedirectOption)
-            await page.on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Select Relative Redirect Options: ${optionValue}`)
-            }).select(xpathRelativeOption, optionValue);
-        }
-
-    },
-
     /**
-     * When you opened the Add Rule form dialog, you can click to show Advanced View
+     * When you are in 'Version Details' page, you can save all changes for this version
      * @param {*} page 
+     * @returns 
      */
-    openAdvancedViewInRuleForm: async (page) => {
-        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="modalButtonDiv"]//button[contains(@class, "switchDetailView")]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Click to 'Show Advanced View'`)
-            }).click()
-    },
-
-    saveRuleDetailsIntoPolicy: async (page) => {
-        await page.locator(`xpath=//div[@id="modalButtonDiv"]//button[@id="saveRule"]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.yellow(`Click to 'Save Rule' button`)
-            }).click();
-    },
-
-    savePolicyChanges: async (page) => {
+    savePolicyVersionChanges: async (page) => {
         await new Promise(r => setTimeout(r, 1000));
 
         const versionName = await page.$eval('xpath=//div[contains(@class, "policyVersionDetails")]//span', el => el.innerText);
@@ -174,88 +186,12 @@ var self = module.exports = {
         }
     },
 
-    updateRuleMatchInAdvancedView: async (page, matchName, matchOperator, matchValue, index) => {
-        const matchPath = `xpath=//div[@id="rule_details_advanced"]//div[contains(@class, "updateableList")]/div[${index}]`
-        const matchNamePath = `${matchPath}//ng-form//select`
-        var optionValue = await page.$$eval(`${matchNamePath}/option`, (options, text) => options.find(o => o.innerText === text).value, matchName)
-        await page.select(matchNamePath, optionValue);
-        log.white(`Select Rule Match Name: ${matchName}`)
-
-        const matchOperatorPath = `${matchPath}//match-operator//select`
-        let operatorOptions = await page.$$eval(`${matchOperatorPath}/option`, (options, text) => options.find(o => o.innerText === text).value, matchOperator)
-        await page.select(matchOperatorPath, operatorOptions);
-        log.white(`Select Rule Match Operator: ${matchOperator}`)
-
-        const matchValuePath = `${matchPath}//match-multi-value//input[@type="text"]`
-        await page.locator(matchValuePath)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill the rule match value: ${matchValue}`)
-            }).fill(matchValue);
-        await page.keyboard.press('Enter')
-
-        //const checkbox1 = `//div[@id="rule_details_advanced"]//div[contains(@class, "updateableList")]/div[1]//match-multi-value//input[@type="checkbox"]/following-sibling::div[contains(string(), "Allow wildcards * (zero or more) and ? (any character)")]`
-    },
-
     /**
-     * Click to menu item in `<ul><li></li></ul>` when open the dropdown from policy item
+     * When you are in 'Version Details' page, you can active staging for this version
      * @param {*} page 
-     * @param {*} dropdownItemText 
+     * @returns 
      */
-    clickToItemInDropdown: async (page, versionNumber, dropdownItemText) => {
-        const xpathTdIcon = `xpath=//akam-table//table//tr[td[@row-property="version"][contains(string(), "${versionNumber}")]]/td[last()]/div`
-        const matchItemPath = `${xpathTdIcon}//ul[@class="dropdown-menu"]/li[contains(string(),"${dropdownItemText}")]`
-        await page.locator(matchItemPath)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Click to item ${dropdownItemText} in dropdown`)
-            })
-            .click();
-    },
-
-    editTheRuleInPolicy: async (page, ruleName) => {
-        await page.locator('xpath=//div[contains(@class, "akamaiTableRules")]//table/tbody/tr').wait();
-
-        const xpthSearchInput = `xpath=//div[contains(@class, "akamaiTableRules")]//input[@type="search"]`
-        await page.locator(xpthSearchInput)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Find the rule with name: '${ruleName}'`)
-            })
-            .fill(ruleName)
-
-        const matchResult = `xpath=//div[contains(@class, "akamaiTableRules")]//table/tbody/tr[td[contains(@class, "name-column")][contains(string(), "${ruleName}")]]/td[last()]/div`
-        await page.locator(matchResult)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Click to cloudlets: '${ruleName}'`)
-            }).click()
-
-        self.clickToItemInDropdownOfRule(page, matchResult, "Edit Rule")
-
-
-    },
-
-    editRedirectUrl: async (page, redirectURL) => {
-        await page.locator(`xpath=//form[@name="ruleForm"]//div[@id="rule-settings-redirect"]//input[@name="redirectURL"]`)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Fill the redirect URL '${redirectURL}'`)
-            }).fill(redirectURL)
-    },
-
-
-
-    /**
-     * Click to menu item in `<ul><li></li></ul>` when open the dropdown from policy item
-     * @param {*} page 
-     * @param {*} dropdownItemText 
-     */
-    clickToItemInDropdownOfRule: async (page, rowContainsMenuXpath, dropdownItemText) => {
-        const matchItemPath = `${rowContainsMenuXpath}//ul[@class="dropdown-menu"]/li[contains(string(),"${dropdownItemText}")]`
-        await page.locator(matchItemPath)
-            .on(puppeteer.LocatorEvent.Action, () => {
-                log.white(`Click to item ${dropdownItemText} in dropdown`)
-            })
-            .click();
-    },
-
-    activeStaging: async (page) => {
+    activeStagingForCurrentVersion: async (page) => {
         const xpathBtn = `xpath=//div[contains(@class, "buttonsRow")]//button[contains(string(), "Activate Version")]`
         await page.locator(xpathBtn).click();
 
@@ -278,7 +214,5 @@ var self = module.exports = {
 
         return versionName;
     }
-
-
 
 }
